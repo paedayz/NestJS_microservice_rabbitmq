abd17021158825f620ac336e7b074a11260426c7
paedayz <pae16phasit@gmail.com>
2022-01-13 22:01:01 +0700

    init main server

--- main/src/main.ts
+++ main/src/main.ts
@@ -3,10 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api')
+  app.setGlobalPrefix('api');
   app.enableCors({
     origin: 'http//:localhost:3000',
   });
-  await app.listen(8000);
+  await app.listen(8001);
 }
 bootstrap();
