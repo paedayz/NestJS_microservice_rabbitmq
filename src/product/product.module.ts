195cb7a8c2c593bcaad279c1236748e9a48dea02
paedayz <pae16phasit@gmail.com>
2022-01-13 20:56:14 +0700

    created get all product service

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductController } from './product.controller';
 import { Product } from './product.entity';
+import { ProductService } from './product.service';
 
 @Module({
   imports: [
       TypeOrmModule.forFeature([Product])
   ],
-  controllers: [ProductController]
+  controllers: [ProductController],
+  providers: [ProductService]
 })
 export class ProductModule {}
