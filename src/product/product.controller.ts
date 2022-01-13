826bab558cefa45b836d98cf34d2a7d3fcf919ef
paedayz <pae16phasit@gmail.com>
2022-01-13 20:57:49 +0700

    created all product query controller

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { ProductService } from './product.service';
 
 @Controller('products')
 export class ProductController {
+    constructor(private productService: ProductService){}
+    
     @Get()
     all() {
-        return 'all product'
+        return this.productService.all()
     }
 }
