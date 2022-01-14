c0284f038285231e738ea5354c90383da2e5cd0e
paedayz <pae16phasit@gmail.com>
2022-01-14 11:24:44 +0700

    connect to mongoDB and auto create schema

--- main/src/app.module.ts
+++ main/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductController } from './product/product.controller';
+import { ProductService } from './product/product.service';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/nest_main', {
+      autoCreate: true,
+    }),
+    ProductModule
+  ],
+  controllers: [AppController, ProductController],
+  providers: [AppService, ProductService],
 })
 export class AppModule {}
